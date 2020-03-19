4d68e9e979548dacfb8e7386a887b30254cbe5da
Levon K. Mkrtchyan <levon.mkrtchyan@gmail.com>
2020-03-19 11:27:55 -0400

    removing dead code

--- src/components/Avro/BaseAvroRecord.ts
+++ src/components/Avro/BaseAvroRecord.ts
@@ -1,25 +1,9 @@
-import * as avro from "avsc";
-import { Type } from "avsc";
-import * as mem from "mem";
 import { AvroRecord } from "./AvroRecord";
-
-function cacheKeyForSchema(schema: any): string {
-    return schema.namespace + schema.name;
-}
-
-const memoizedTypeForSchema = mem(
-    (schema: any): Type => avro.Type.forSchema(schema),
-    { cacheKey: cacheKeyForSchema },
-);
 
 export abstract class BaseAvroRecord implements AvroRecord {
 
     public static readonly subject: string = "";
     public static readonly schema = {};
-
-    public static getTypeForSchema(schema: any): Type {
-        return memoizedTypeForSchema(schema);
-    }
 
     public abstract schema(): any;
 
