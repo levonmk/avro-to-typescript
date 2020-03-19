bc70658354eb94ed9a8535c9a819bc8e0a29c4fb
Levon K. Mkrtchyan <levon.mkrtchyan@gmail.com>
2020-03-19 09:49:14 -0400

    replace typescript-memoize with mem, comment out createTypeResolver

--- src/components/Avro/BaseAvroRecord.ts
+++ src/components/Avro/BaseAvroRecord.ts
@@ -1,26 +1,35 @@
 import * as avro from "avsc";
-import { Schema, Type } from "avsc";
-import { Memoize } from "typescript-memoize";
+import { Type } from "avsc";
+import * as mem from "mem";
 import { AvroRecord } from "./AvroRecord";
+
+function cacheKeyForSchema(schema: any): string {
+    return schema.namespace + schema.name;
+}
+
+const memoizedTypeForSchema = mem(
+    (schema: any): Type => avro.Type.forSchema(schema),
+    { cacheKey: cacheKeyForSchema },
+);
 
 export abstract class BaseAvroRecord implements AvroRecord {
 
     public static readonly subject: string = "";
     public static readonly schema = {};
 
-    @Memoize((schema: any) => {
-        return schema.namespace + schema.name;
-    })
     public static getTypeForSchema(schema: any): Type {
-        return avro.Type.forSchema(schema);
+        return memoizedTypeForSchema(schema);
     }
 
+    /*
+    I don't understand how this works, or where it's used
     @Memoize((schema: any) => {
         return schema.namespace + schema.name;
     })
     public static createTypeResolver(baseType: Type, newType: Type): Type {
         return baseType.createResolver(newType) as Type;
     }
+     */
 
     public abstract schema(): any;
 
